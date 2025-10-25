92707a87486d4cf963127315ffa63468de732314
Hakuna <4235362@gmail.com>
2025-10-26 00:59:56 +0800

    Keep calm and commit

--- src/app/(article)/posts/[id]/page.tsx
+++ src/app/(article)/posts/[id]/page.tsx
@@ -41,6 +41,8 @@ const asdada123s1: boolean = 123123
 const asdada123s2: boolean = 123123
 const asdada123s3: boolean = 123123
 const asdada123s5: boolean = 123123
+const asdada123s5234: boolean = 123123
+const asdada123s52342: boolean = 123123
 export const generateMetadata = async ({ params }: PageProps) => {
   const { id } = params
 
