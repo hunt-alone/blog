55f4cf688e6de547aa9381ddaff5a15a169d182c
HaKuna Matata <html@HaKunas-MacBook-Pro.local>
2025-10-25 23:57:14 +0800

    调整 markdown 样式

--- src/app/(article)/posts/[id]/page.tsx
+++ src/app/(article)/posts/[id]/page.tsx
@@ -69,6 +69,8 @@ export default async function Page({ params }: PageProps) {
 
   const currentIndex = nodes.findIndex(node => node.number === number)
   const previousPost =
+
+  
     currentIndex !== -1 ? nodes[currentIndex + 1] ?? null : null
   const nextPost = currentIndex > 0 ? nodes[currentIndex - 1] ?? null : null
 
