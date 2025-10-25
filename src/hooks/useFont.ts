a597a254628a648bb48f7e8323f51ac687e62202
HaKuna Matata <html@HaKunas-MacBook-Pro.local>
2025-10-25 21:51:09 +0800

    屏蔽 eslint中和服务端的冲突

--- src/hooks/useFont.ts
+++ src/hooks/useFont.ts
@@ -1,5 +1,5 @@
-import { use } from 'react'
+import { useContext } from 'react'
 
 import { fontContext } from '@/context/font'
 
-export const useFont = () => use(fontContext)
+export const useFont = () => useContext(fontContext)
